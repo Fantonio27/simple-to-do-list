2c6c6a31d533368e27bb64a8b010ddde48c6715d
Francis <francis.antonio0927@gmail.com>
2025-04-12 22:25:48 +0800

    add mock data for task management with icons and statuses

---
+++ assets/json/mock-data.ts
@@ -0,0 +1,25 @@
+const data = [
+  {
+    icon: "alarm-clock",
+    name: "Task in Progress",
+    status: "pending",
+  },
+  {
+    icon: "weight-lifter",
+    name: "Task in Completed",
+    status: "success",
+  },
+  {
+    icon: "hot-beverage",
+    name: "Task in Won't Do",
+    status: "unsuccess",
+  },
+  {
+    icon: "books",
+    name: "Task To Do",
+    description: "Work on a Challenge on devCallenges.io, learn TypeScript.",
+    status: "",
+  },
+];
+
+export default data ;
